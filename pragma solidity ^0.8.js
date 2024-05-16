3eacf55649967dcd7b752c138c49a592f8305876
quirpyty <168150786+quirpyty@users.noreply.github.com>
2024-05-17 01:17:37 +0800

    Add files via upload

---
+++ pragma solidity ^0.8.js
@@ -0,0 +1,31 @@
+pragma solidity ^0.8.0;
+
+contract MyToken {
+    string public name;
+    string public symbol;
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+
+    event Mint(address indexed to, uint256 value);
+    event Burn(address indexed from, uint256 value);
+
+    constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _totalSupply;
+        balanceOf[msg.sender] = _totalSupply; // Assign all tokens to contract deployer
+    }
+
+    function mint(address _to, uint256 _value) public {
+        totalSupply += _value;
+        balanceOf[_to] += _value;
+        emit Mint(_to, _value);
+    }
+
+    function burn(address _from, uint256 _value) public {
+        require(balanceOf[_from] >= _value, "Insufficient balance");
+        totalSupply -= _value;
+        balanceOf[_from] -= _value;
+        emit Burn(_from, _value);
+    }
+}
